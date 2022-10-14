2031572813de9612111f4fefbaad38d97d35fae1
unknown <ishpreetkaur2001@gmail.com>
2022-10-15 00:15:48 +0530

    Show GraphQL data on index page

---
+++ src/routes/+page.svelte
@@ -0,0 +1,38 @@
+<script context="module">
+      +import { client } from '$lib/graphql-client'
+      -import { gql, GraphQLClient } from 'graphql-request'
+      +import { gql } from 'graphql-request'
+    
+      export const load = async () => {
+    -   const client = new GraphQLClient(import.meta.env.VITE_GRAPHQL_API)
+    
+        const query = gql`
+          query GetProjects {
+            projects {
+              name
+              slug
+              description
+              demo
+              sourceCode
+              image {
+                url
+              }
+            }
+          }
+        `
+    
+        const { projects } = await client.request(query)
+    
+        return {
+          props: {
+            projects,
+          },
+        }
+      }
+    </script>
+    
+    <script>
+      export let projects
+    </script>
+    
+    <pre>{JSON.stringify(projects, null, 2)}</pre>
