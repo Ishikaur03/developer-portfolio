bf81462bd6a7b8bbd62432c3bd43e35ac763044f
unknown <ishpreetkaur2001@gmail.com>
2022-10-16 21:59:57 +0530

    Add project page using SvelteKit routing

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,8 @@
 <script>
-	import '../app.css'
-  </script>
-  
-  <main class="container max-w-3xl mx-auto px-4 mb-20">
+	import '../app.postcss';
+
+</script>
+
+<main class="container max-w-3xl mx-auto px-4 mb-20">
 	<slot />
-  </main>
+</main>
--- svelte.config.js
+++ svelte.config.js
@@ -1,17 +1,24 @@
-import preprocess from 'svelte-preprocess';
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-auto'
+import path from 'path'
+import preprocess from 'svelte-preprocess'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		adapter: adapter()
-	},
+  kit: {
+    adapter: adapter(),
 
-	preprocess: [
-		preprocess({
-			postcss: true
-		})
-	]
-};
+    alias: {
+      $components: path.resolve('./src/lib/components'),
+      $lib: path.resolve('./src/lib'),
+      $stores: path.resolve('./src/stores'),
+    },
+  },
 
-export default config;
+  preprocess: [
+    preprocess({
+      postcss: true,
+    }),
+  ],
+}
+
+export default config
