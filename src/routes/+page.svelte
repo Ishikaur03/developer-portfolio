9ec1ba66963d81db3cd88b06265b29bc9de140c7
unknown <ishpreetkaur2001@gmail.com>
2022-10-17 01:22:13 +0530

    Add posts index page and slug page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -29,11 +29,11 @@
     <title>My Portfolio project</title>
   </svelte:head>
   
-  <h1 class="font-bold text-center mb-20 text-5xl">
+   <h1 class="font-bold text-center mb-20 text-5xl">
     Welcome to my Portfolio
   </h1>
   
-  {#each authors as { name, intro, picture: { url } }}
+ {#each authors as { name, intro, picture: { url }}}
     <div class="flex mb-40 items-end">
       <div class="mr-6">
         <h2 class="text-3xl mb-4 font-bold tracking-wider">{name}</h2>
@@ -42,7 +42,7 @@
   
       <img class="mask mask-squircle h-48" src={url} alt={name} />
     </div>
-  {/each}
+   {/each}
   
   <div
     class="grid gap-10 md:grid-cols-4 md:px-10 lg:grid-cols-6 lg:-mx-52"
