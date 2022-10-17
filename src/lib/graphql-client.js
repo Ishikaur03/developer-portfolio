bff376b6ea508e59115b11be0bdb608b4d38ffb8
unknown <ishpreetkaur2001@gmail.com>
2022-10-18 00:29:40 +0530

    Add navbar component

--- src/lib/graphql-client.js
+++ src/lib/graphql-client.js
@@ -1,4 +1,7 @@
-import { GraphQLClient } from 'graphql-request'
+/*import { GraphQLClient } from 'graphql-request'
 const GRAPHQL_ENDPOINT = import.meta.env.VITE_GRAPHQL_API
 
-export const client = new GraphQLClient(GRAPHQL_ENDPOINT)
+export const client = new GraphQLClient(GRAPHQL_ENDPOINT)*/
+import { GraphQLClient } from 'graphql-request'
+const HYGRAPH_ENDPOINT = import.meta.env.VITE_HYGRAPH_ENDPOINT
+export const client = new GraphQLClient(HYGRAPH_ENDPOINT)
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,11 @@
 <script>
-	import '../app.postcss';
-
-</script>
-
-<main class="container max-w-3xl mx-auto px-4 mb-20">
+	import Footer from '$lib/components/footer.svelte'
+	import Navbar from '$lib/components/navbar.svelte'
+	import '../app.css'
+  </script>
+  
+  <Navbar />
+  <main class="container max-w-3xl mx-auto px-4 mb-20">
 	<slot />
-</main>
+  </main>
+  <Footer />
